06cbb144ab0abe036ea7acb219b1d61994cb476a
surbhigoel555 <surbhigoel555@gmail.com>
2024-12-02 18:48:34 +0530

    Header, Footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © {new Date().getFullYear()}{" "}
+          <a href="/" className="hover:underline">
+            Movies
+          </a>
+          . All Rights Reserved.
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+          <li>
+            <NavLink to="/" className="hover:underline me-4 md:me-6">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="movies/popular" className="hover:underline me-4 md:me-6">
+              Popular
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="movies/top" className="hover:underline me-4 md:me-6">
+              Top
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="movies/upcoming" className="hover:underline">
+              Upcoming
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
