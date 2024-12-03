48004ea9bc6fd1dfc03c1a1f9e6381a29a07c5aa
surbhigoel555 <surbhigoel555@gmail.com>
2024-12-03 13:48:00 +0530

    scrollToTop Added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { useLocation } from "react-router-dom";
 import "./App.css";
 import { Footer, Header } from "./components";
 import AllRoutes from "./routes/AllRoutes";
+import { useEffect } from "react";
 
 function App() {
+
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className="App dark:bg-darkbg">
       <Header />
